da7e79657d612c9abb30dfe6b387591b4c165bfe
yash saini <ys3853428@gmail.com>
2024-02-17 00:50:18 +0530

    Modified Py-Script

--- Project 1 - Telecom/Telecom - Statistics  Analysis.py
+++ Project 1 - Telecom/Telecom - Statistics  Analysis.py
@@ -16,7 +16,7 @@ def count_values(dataframe, column_list):
         print("-" * 20)
 
 l = ["Blue", "Wi_Fi", "Tch_Scr", "Ext_Mem"]
-count_values(df,l) 
+print(count_values(df,l)) 
 
 
 ### The children want phones that have the following: Bluetooth, WiFi, touch screen and external memory support
@@ -25,7 +25,7 @@ con1 = (df['Blue'] == 'yes') & (df['Wi_Fi'] == 'yes') & (df['Tch_Scr'] == 'yes')
 print(con1.head())
 
 l2= ['Px_h','Px_w']
-count_values(df,l2)
+print(count_values(df,l2))
 
 df['Px'] = df['Px_h'] + df['Px_w']
 print(df['Px'].value_counts)()
@@ -55,51 +55,12 @@ median_resolution = df['Px'].median()
 con2 = df['Px'] >= median_resolution
 print(con2.head())
 
-
-
-# Observations:
-# 
-# The features "Px_h" and "Px_w" are respectively the number of pixels in the phone screen in the vertical and horizontal axes.
-# 
-# We created a new feature called "Px" which is the product of the features "Px_h" and "Px_w".
-# 
-# The median has been selected as a threshold in this case.
-# 
-# In case it is too strict, we can choose the mean as a threshold.
-
-# # Task 4 - Obtain the logical conditions for the features "Scr_h" and "Scr_w"
-
-# In[22]:
-
-
-# Let's tackle these features: "Scr_h", "Scr_w"
 l3 = ['Scr_h','Scr_w']
-
-
-# In[23]:
-
-
-count_values(df,l3)
-
-
-# In[24]:
-
+print(count_values(df,l3))
 
 # Create a new feature called "Scr_d" which stores the length of the diagonal of the screen of the phone
 df['Scr_d'] = np.sqrt(df['Scr_h']**2 + df['Scr_w']**2)
-
-
-# In[25]:
-
-
 df['Scr_d'].head()
-
-
-# ### Create a histogram of the "Scr_d" feature and also show the quartiles
-# 
-
-# In[26]:
-
 
 # Plotting the histogram
 plt.figure(figsize=(10, 6))
@@ -107,7 +68,6 @@ plt.hist(df['Scr_d'], bins=20, color='skyblue', edgecolor='black')
 plt.title('Histogram of Scr_d Feature')
 plt.xlabel('Length of Diagonal of the Screen (Scr_d)')
 plt.ylabel('Frequency')
-
 # Adding quartile lines
 q25 = df['Scr_d'].quantile(0.25)
 q50 = df['Scr_d'].quantile(0.50)
@@ -119,56 +79,20 @@ plt.legend()
 plt.show()
 
 
-# ### The children want phones that have very good screen sizes
-# ### Consider the phones that have screen sizes greater than or equal to the upper quartile value in the data set
-# ### Create a logical condition for this situation and store the logical values as "con3"
-
-# In[27]:
-
+### The children want phones that have very good screen sizes
+### Consider the phones that have screen sizes greater than or equal to the upper quartile value in the data set
+### Create a logical condition for this situation and store the logical values as "con3"
 
 # Calculate the upper quartile value for the Scr_d feature
 upper_quartile = df['Scr_d'].quantile(0.75)
-
 # Create the logical condition con3
 con3 = df['Scr_d'] >= upper_quartile
 
-
-# In[28]:
-
-
-con3.head()
-
-
-# Observations:
-# 
-# The features "Scr_h" and "Scr_w" are respectively the height and the width of the phone screen.
-# 
-# We created a new feature called "Scr_d" which is essentially the length of the screen diagonal.
-# 
-# The upper quartile has been selected as a threshold in this case as the children were very particular on this point.
-# 
-# In case it is too strict, we can choose the mean or the median as a threshold.
-
-# # Task 5 - Obtain the logical conditions for the features "PC" and "FC"
-
-# In[29]:
-
+print(con3.head())
 
 # Let's tackle these features: "PC", "FC"
 l4 = ['PC','FC']
-
-
-# In[30]:
-
-
-count_values(df,l4)
-
-
-# ### Create a histogram of the "PC" feature and also show the mean and the median
-# 
-
-# In[31]:
-
+print(count_values(df,l4))
 
 # Plotting the histogram
 plt.figure(figsize=(10, 6))
@@ -176,7 +100,6 @@ plt.hist(df['PC'], bins=20, color='skyblue', edgecolor='black')
 plt.title('Histogram of PC Feature')
 plt.xlabel('Primary Camera Resolution (PC)')
 plt.ylabel('Frequency')
-
 # Adding mean and median lines
 mean_value = df['PC'].mean()
 median_value = df['PC'].median()
@@ -186,21 +109,12 @@ plt.legend()
 plt.show()
 
 
-# ### Create a histogram of the "FC" feature and also show the mean and the median
-# 
-# 
-# 
-
-# In[32]:
-
-
 # Plotting the histogram
 plt.figure(figsize=(10, 6))
 plt.hist(df['FC'], bins=20, color='skyblue', edgecolor='black')
 plt.title('Histogram of FC Feature')
 plt.xlabel('Front camera resolution (in MP) (FC)')
 plt.ylabel('Frequency')
-
 # Adding mean and median lines
 mean_value = df['FC'].mean()
 median_value = df['FC'].median()
@@ -210,50 +124,18 @@ plt.legend()
 plt.show()
 
 
-# ### The children want phones that have good primary and front camera resolutions
-# ### Consider the phones that have primary and front camera resolutions greater than or equal to their respective mean values
-# ### Create a logical condition for this situation and store the logical values as "con4"
-
-# In[33]:
-
-
 # Calculate the mean values for PC and FC features
 mean_pc = df['PC'].mean()
 mean_fc = df['FC'].mean()
-
 # Create the logical condition con4
 con4 = (df['PC'] >= mean_pc) & (df['FC'] >= mean_fc)
-
-
-# In[34]:
-
-
-con4.head()
-
-
-# Observations:
-# 
-# The features "PC" and "FC" are respectively the resolutions of the primary camera and the front camera.
-# 
-# The respective means have been selected as thresholds in this case.
-# 
-# In case it is too strict, we can choose the respective medians as thresholds.
-
-# # Task 6 - Obtain the logical conditions for the features "Int_Mem", "Bty_Pwr" and "RAM"
-
-# In[35]:
-
+print(con4.head())
 
 # Let's tackle these features: "Int_Mem", "Bty_Pwr", "RAM"
 l5 = ["Int_Mem", "Bty_Pwr", "RAM"]
-count_values(df,l5)
+print(count_values(df,l5))
 
-
-# ### Create a histogram of the "Int_Mem" feature and also show the mean and the median
-
-# In[36]:
-
-
+# Plotting the histogram
 plt.figure(figsize=[12,6])
 plt.hist(df['Int_Mem'], bins=20,color='skyblue',edgecolor='black')
 plt.title('Histogram of Int_Mem feature')
